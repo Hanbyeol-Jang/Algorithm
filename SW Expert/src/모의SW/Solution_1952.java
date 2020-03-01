cba4f542a9c0e88635541c5976005fd3601d2b49
Hanbyeol-Jang <35832112+Hanbyeol-Jang@users.noreply.github.com>
2020-03-01 18:20:09 +0900

    1952 dfs

--- SW Expert/src/모의SW/Solution_1952.java
+++ SW Expert/src/모의SW/Solution_1952.java
@@ -1,7 +1,5 @@
 package 모의SW;
 
-import java.util.Collections;
-import java.util.LinkedList;
 import java.util.Scanner;
 
 public class Solution_1952 {
@@ -9,13 +7,7 @@ public class Solution_1952 {
 	static int TestCase;
 	static int[] price;
 	static int[] month;
-	static int day;
-	static int min;
-
-	static boolean[] isc;
-
-	static LinkedList<Integer> monthList;
-	static LinkedList<Integer> costList;
+	static int cost;
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
@@ -24,76 +16,32 @@ public class Solution_1952 {
 		for (int T = 1; T <= TestCase; T++) {
 			price = new int[4];
 			month = new int[13];
-			monthList = new LinkedList<>();
-			costList = new LinkedList<>();
-			min = Integer.MAX_VALUE;
-			day = 0;
-
 			for (int i = 0; i < 4; i++) {
 				price[i] = sc.nextInt();
 			}
-			for (int i = 1; i < 13; i++) {
+			for (int i = 1; i <= 12; i++) {
 				month[i] = sc.nextInt();
-				if (month[i] != 0) {
-					day += month[i];
-					monthList.add(i);
-				}
 			}
 
-//			for (int i = 0; i < monthList.size(); i++) {
-//				System.out.print(monthList.get(i) + " ");
-//			}
-//			System.out.println();
-
-			isc = new boolean[monthList.size()];
-
-			for (int i = 0; i <= monthList.size(); i++) {
-				combi(monthList.size(), i, 0, 0);
-			}
-
-			costList.add(price[3]);
-
-//			for (int i = 0; i < costList.size(); i++) {
-//				System.out.print(costList.get(i) + " ");
-//			}
-//			System.out.println();
-
-			Collections.sort(costList);
-
-			System.out.println("#" + T + " " + costList.get(0));
+			cost = Integer.MAX_VALUE;
+			dfs(1, 0);
+			System.out.println("#" + T + " " + cost);
 		}
 	}
 
-	public static void combi(int n, int r, int start, int count) {
-		if (count == r) {
-			int sum = 0;
-
-			for (int i = 0; i < isc.length; i++) {
-				if (isc[i]) {
-					System.out.print(monthList.get(i) + " ");
-					sum += month[monthList.get(i)] * price[0];
-				}
+	public static void dfs(int i, int sum) {
+		if (i > 12) {
+			if (cost > sum) {
+				cost = sum;
 			}
-			System.out.print(" ||  ");
-			for (int i = 0; i < isc.length; i++) {
-				if (!isc[i]) {
-					System.out.print(monthList.get(i) + " ");
-					sum += price[1];
-				}
-			}
-			System.out.println(" ||  " + sum);
-			costList.add(sum);
 			return;
 		}
 
-		for (int i = start; i < n; i++) {
-			if (!isc[i]) {
-				isc[i] = true;
-				combi(n, r, i + 1, count + 1);
-				isc[i] = false;
-			}
-		}
+		dfs(i + 1, sum + month[i] * price[0]);
+		dfs(i + 1, sum + price[1]);
 
+		dfs(i + 3, sum + price[2]);
+
+		dfs(i + 12, sum + price[3]);
 	}
-
 }
