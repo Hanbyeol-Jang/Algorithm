3b326440b98e8a5863b0a60e4ef9e67de11c9228
Hanbyeol-Jang <35832112+Hanbyeol-Jang@users.noreply.github.com>
2020-05-06 23:25:18 +0900

    Create Main_17472.java

---
+++ BAEKJOON/src/Main_17472.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Main_17472 {
+
+	static int N, M;
+	static int[][] map;
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		N = sc.nextInt();
+		M = sc.nextInt();
+		map = new int[N][M];
+
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < M; j++) {
+				map[i][j] = sc.nextInt();
+			}
+		}
+		
+		printMap(map);
+	}
+
+	public static void printMap(int[][] m) {
+		for (int i = 0; i < m.length; i++) {
+			for (int j = 0; j < m[i].length; j++) {
+				System.out.print(m[i][j] + " ");
+			}
+			System.out.println();
+		}
+		System.out.println("======================");
+	}
+}
