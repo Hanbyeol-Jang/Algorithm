f4438da8877413a33c60d266c8bb67a3fba2466d
Hanbyeol-Jang <35832112+Hanbyeol-Jang@users.noreply.github.com>
2021-05-05 00:39:17 +0900

    Update Solution_전화번호목록.java

--- Programmers/src/Solution_전화번호목록.java
+++ Programmers/src/Solution_전화번호목록.java
@@ -1,31 +1,36 @@
+import java.util.ArrayList;
 import java.util.Collections;
-import java.util.LinkedList;
 
 public class Solution_전화번호목록 {
 
 	public static void main(String[] args) {
-		String[] phone_book = { "97674223", "1195524421", "119" };
+//		String[] phone_book = { "119", "97674223", "1195524421" };
+//		String[] phone_book = { "123", "456", "789" };
+//		String[] phone_book = { "12", "123", "1235", "567", "88" };
+		String[] phone_book = { "123123010", "010", "10" };
+
 		System.out.println(solution(phone_book));
 	}
 
 	public static boolean solution(String[] phone_book) {
-
-		LinkedList<String> list = new LinkedList<>();
+		boolean answer = true;
+		ArrayList<String> list = new ArrayList<>();
 
 		for (int i = 0; i < phone_book.length; i++) {
 			list.add(phone_book[i]);
 		}
 
-		Collections.sort(list);
+		Collections.sort(list, (o1, o2) -> {
+			return o1.compareTo(o2);
+		});
 
 		for (int i = 0; i < list.size() - 1; i++) {
-
-			if (list.get(i + 1).contains(list.get(i))) {
-				return false;
+			if (list.get(i + 1).indexOf(list.get(i)) == 0) {
+				answer = false;
+				break;
 			}
 		}
 
-		boolean answer = true;
 		return answer;
 	}
 }
