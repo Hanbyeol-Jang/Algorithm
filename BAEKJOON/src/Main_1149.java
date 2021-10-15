f3b24c3c2970d7b1fc57726e8a92391604e5e11a
Hanbyeol-Jang <35832112+Hanbyeol-Jang@users.noreply.github.com>
2021-10-15 20:58:54 +0900

    Create Main_1149.java

---
+++ BAEKJOON/src/Main_1149.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_1149 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+
+        int[][] arr = new int[N][3];
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < 3; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        int[][] dp = new int[N][3];
+
+        dp[0][0] = arr[0][0];
+        dp[0][1] = arr[0][1];
+        dp[0][2] = arr[0][2];
+
+        for (int i = 1; i < N; i++) {
+            dp[i][0] = Math.min(dp[i - 1][1], dp[i - 1][2]) + arr[i][0];
+            dp[i][1] = Math.min(dp[i - 1][0], dp[i - 1][2]) + arr[i][1];
+            dp[i][2] = Math.min(dp[i - 1][0], dp[i - 1][1]) + arr[i][2];
+        }
+
+        int min = Integer.MAX_VALUE;
+
+        for (int i = 0; i < 3; i++) {
+            min = Math.min(min, dp[N - 1][i]);
+        }
+
+        System.out.println(min);
+    }
+}
