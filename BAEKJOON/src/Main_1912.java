2c7fd9fc725368d58c22a8a9533872968fc52671
Hanbyeol-Jang <35832112+Hanbyeol-Jang@users.noreply.github.com>
2021-10-19 19:44:40 +0900

    Create Main_1912.java

---
+++ BAEKJOON/src/Main_1912.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_1912 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int max = arr[0];
+
+        for (int i = 1; i < n; i++) {
+            arr[i] = Math.max(arr[i - 1] + arr[i], arr[i]);
+            max = Math.max(max, arr[i]);
+        }
+
+        System.out.println(max);
+    }
+}
